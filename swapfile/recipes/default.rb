f3fdf7ec602d247349527ea74c9f750b1c71c836
Rodrigo Justino da Costa <rojcosta@gmail.com>
2017-10-09 11:08:43 -0300

    Altera o nome do swapfile

--- swapfile/recipes/default.rb
+++ swapfile/recipes/default.rb
@@ -1,17 +1,17 @@
 Chef::Log.info("********** Executing cron recipe! **********")
-script 'create swapfile' do
+script 'create chef-swapfile' do
   interpreter 'bash'
-  not_if { File.exists?('/var/swapfile') }
+  not_if { File.exists?('/var/chef-swapfile') }
   code <<-eof
-    dd if=/dev/zero of=/var/swapfile bs=1M count=2048 &&
-    chmod 600 /var/swapfile &&
-    mkswap /var/swapfile
+    dd if=/dev/zero of=/var/chef-swapfile bs=1M count=2048 &&
+    chmod 600 /var/chef-swapfile &&
+    mkswap /var/chef-swapfile
   eof
 end
 
 mount '/dev/null' do  # swap file entry for fstab
   action :enable  # cannot mount; only add to fstab
-  device '/var/swapfile'
+  device '/var/chef-swapfile'
   fstype 'swap'
 end
 
